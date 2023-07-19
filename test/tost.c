582c48389094402a65b196fc19f318af77846a7d
bentraxgel <dhrtpdnjs@gmail.com>
2023-07-20 04:18:44 +0900

    2023년 7월 20일 목요일 04시 18분 44초 KST

--- test/tost.c
+++ test/tost.c
@@ -7,7 +7,8 @@ extern char **environ;
 int    main(int ac, char **av, char **envp)
 {
     // execve("print_envp", NULL, environ);
-    execve("print_envp", NULL, envp);
+    // execve("print_envp", NULL, envp);
+    execve("print_envp", NULL, NULL);
     perror(NULL);
     return (1);
 }
